323ea49519f938e0ee1a8f5f6bfd2a1c0a6f1062
kylekaracadag <kylekaracadag1@gmail.com>
2021-05-08 17:39:07 -0400

    Made button clickable

--- app/src/main/java/com/example/goalmanager/MainActivity.java
+++ app/src/main/java/com/example/goalmanager/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.goalmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button goalButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+
+        goalButton = findViewById(R.id.addGoalButton);
+        goalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddGoalActivity.class));
+            }
+        });
     }
 }
