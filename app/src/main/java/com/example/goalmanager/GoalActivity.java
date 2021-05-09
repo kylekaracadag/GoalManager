4149bfebd667477d63054e8ecae510c9cdade4f5
kylekaracadag <kylekaracadag1@gmail.com>
2021-05-09 02:46:02 -0400

    Added list view of goals

--- app/src/main/java/com/example/goalmanager/GoalActivity.java
+++ app/src/main/java/com/example/goalmanager/GoalActivity.java
@@ -1,20 +1,32 @@
 package com.example.goalmanager;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class GoalActivity extends AppCompatActivity {
+    private static final String FILE_NAME = "input.txt";
 
     Button backBttn;
     Button saveBttn;
     EditText goalText;
+    String tmpText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +44,12 @@ public class GoalActivity extends AppCompatActivity {
         saveBttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String tmpText = goalText.getText().toString();
+                tmpText = goalText.getText().toString();
+                setText(GoalActivity.this, tmpText);
+                writeFile();
+//                Intent intent = new Intent(GoalActivity.this, MainActivity.class);
+//                intent.putExtra("tmp", tmpText);
+//                startActivity(intent);
             }
         });
 
@@ -42,6 +59,27 @@ public class GoalActivity extends AppCompatActivity {
                 startActivity(new Intent(GoalActivity.this, MainActivity.class));
             }
         });
+    }
+
+    public void writeFile() {
+        String textToSave = tmpText;
+
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("input1.txt", MODE_PRIVATE);
+            fileOutputStream.write(textToSave.getBytes());
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setText(Context context, String text) {
+        SharedPreferences prefs = context.getSharedPreferences("myAppPackage", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("txt", text);
+        editor.commit();
     }
 }
 
--- app/src/main/java/com/example/goalmanager/MainActivity.java
+++ app/src/main/java/com/example/goalmanager/MainActivity.java
@@ -5,17 +5,27 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Goal> list;
     private RecyclerView recyclerView;
+    private GoalActivity goalActivity;
     GoalAdapter goalAdapter;
     Button goalButton;
 
@@ -32,10 +42,14 @@ public class MainActivity extends AppCompatActivity {
         list = new ArrayList<>();
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        list.add(new Goal("Test One"));
-        list.add(new Goal("Test Two"));
-        list.add(new Goal("Test Three"));
-        list.add(new Goal("Test Four"));
+        String text = getText(MainActivity.this);
+        readFile();
+//        list.add(new Goal(text));
+
+//        list.add(new Goal("Test One"));
+//        list.add(new Goal("Test Two"));
+//        list.add(new Goal("Test Three"));
+//        list.add(new Goal("Test Four"));
 
         goalAdapter = new GoalAdapter(MainActivity.this, list);
         recyclerView.setAdapter(goalAdapter);
@@ -48,5 +62,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void readFile() {
+        try {
+            FileInputStream fileInputStream = openFileInput("input1.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String lines;
+            while ((lines = bufferedReader.readLine()) != null) {
+                stringBuffer.append(lines + "\n");
+                list.add(new Goal(stringBuffer.toString()));
+            }
+
+//            list.add(new Goal(stringBuffer.toString()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getText(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("myAppPackage", 0);
+        return prefs.getString("txt", "");
     }
 }
